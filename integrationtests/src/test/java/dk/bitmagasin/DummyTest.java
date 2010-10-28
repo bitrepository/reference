b83d999fc55fa0f04ea61792cf9f6397806eb605
mikis <mikis@c5b8cdcd-6091-0410-b920-e50e972e9166>
2010-10-28 10:04:21 +0000

    BITMAG-11: Add dummy TestNG testcase
    git-svn-id: https://sbforge.org/svn/bitrepository/trunk@33 c5b8cdcd-6091-0410-b920-e50e972e9166

---
+++ integrationtests/src/test/java/dk/bitmagasin/DummyTest.java
@@ -0,0 +1,15 @@
+package dk.bitmagasin;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+
+public class DummyTest {
+	
+	@Test(groups = { "functest", "checkintest" })
+	public void trivialTest() {
+		Assert.assertEquals(5,5);
+		assertEquals("5 not equal to 3",5, 3);
+	}
+
+}
