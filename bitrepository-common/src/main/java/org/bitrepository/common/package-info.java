bfb1c89c2331d97ebdcd6534a59c93d55440d15b
mikis <mikis@c5b8cdcd-6091-0410-b920-e50e972e9166>
2011-03-11 12:07:05 +0000

    BITMAG-41: Added missing header
    git-svn-id: https://sbforge.org/svn/bitrepository/trunk@108 c5b8cdcd-6091-0410-b920-e50e972e9166

--- bitrepository-common/src/main/java/org/bitrepository/common/package-info.java
+++ bitrepository-common/src/main/java/org/bitrepository/common/package-info.java
@@ -1,3 +1,27 @@
+/*
+ * #%L
+ * bitrepository-common
+ * 
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2010 - 2011 The State and University Library, The Royal Library and The State Archives, Denmark
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
 /**
  * The Commons module contains the functionality used across all the Bitrepository modules.<p>
  * 
