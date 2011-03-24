bc5849e10253b592aac26bd611d22116ca9c1f82
mikis <mikis@c5b8cdcd-6091-0410-b920-e50e972e9166>
2011-03-24 14:11:00 +0000

    BITMAG-175: Add precommit QA script: Added filtering of files to svn propset
    git-svn-id: https://sbforge.org/svn/bitrepository/trunk@121 c5b8cdcd-6091-0410-b920-e50e972e9166

--- precommit.sh
+++ precommit.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Precommit script for checking everything is ready for a commit together with update functionality for trivial stuff.
 
-echo "Fixing svn properties on files"
-svn propset svn:keywords "URL Revision Author Date Id" -R -q *
+# Find files named *.java and *.xml in the src folders and set keywords on these
+find . \( -iname "*.xml" -or -iname "*.java" \) -and -path "*src*" -exec svn propset svn:keywords 'URL Revision Author Date Id' {} -q \;
 
 echo "Updating file headers"
-mvn license:update-file-header -q
+#mvn license:update-file-header -q
 
 echo "Updating project license"
-mvn license:update-project-license -q
+#mvn license:update-project-license -q
 
 echo "Updating third party licenses"
-mvn license:add-third-party -q
+#mvn license:add-third-party -q
 
 echo "Running regression test"
-mvn clean install -q
+#mvn clean install -q
