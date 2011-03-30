c10e83bd158566d80ad1b100a03bfbbb27e0b54d
mikis <mikis@c5b8cdcd-6091-0410-b920-e50e972e9166>
2011-03-30 07:26:29 +0000

    BITMAG-175: Add precommit QA script: Fixed issues from review CR-BITMAG-14
    git-svn-id: https://sbforge.org/svn/bitrepository/trunk@130 c5b8cdcd-6091-0410-b920-e50e972e9166

--- precommit.sh
+++ precommit.sh
@@ -2,16 +2,16 @@
 # Precommit script for checking everything is ready for a commit together with update functionality for trivial stuff.
 
 # Find files named *.java and *.xml in the src folders and set keywords on these
-find . \( -iname "*.xml" -or -iname "*.java" \) -and -path "*src*" -exec svn propset svn:keywords 'URL Revision Author Date Id' {} -q \;
+find . \( -iname "*.xml" -or -iname "*.java" -or -iname "*.properties" -or -iname "*.xsd" \) -and -path "*/src/*" -exec svn propset svn:keywords 'URL Revision Author Date Id' {} -q \;
 
 echo "Updating file headers"
-#mvn license:update-file-header -q
+mvn license:update-file-header -q
 
 echo "Updating project license"
-#mvn license:update-project-license -q
+mvn license:update-project-license -q
 
 echo "Updating third party licenses"
-#mvn license:add-third-party -q
+mvn license:add-third-party -q
 
 echo "Running regression test"
-#mvn clean install -q
+mvn clean install -q
