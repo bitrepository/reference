9ec41f98825b4f30c62bcb6ab67b27b5e59f1f36
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2012-02-16 16:13:30 +0100

    Tuning project license information. This includes: switching to use the codehaus license plugin, added headers to xsd files and remove addition of svnkeywords (as we are now using git).

--- precommit.sh
+++ precommit.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Precommit script for checking everything is ready for a commit together with update functionality for trivial stuff.
 
-# Find files named *.java and *.xml in the src folders and set keywords on these
-find . \( -iname "*.xml" -or -iname "*.java" -or -iname "*.properties" -or -iname "*.xsd" \) -and -path "*/src/*" -exec svn propset svn:keywords 'URL Revision Author Date Id' {} -q \;
-
 echo "Updating file headers"
 mvn license:update-file-header -q
 
