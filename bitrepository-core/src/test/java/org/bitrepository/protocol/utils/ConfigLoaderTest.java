c28a5fb02ab4adfdc0711d250a9341b2ae55d793
jolf <jolf@kb.dk>
2012-05-30 12:45:19 +0200

    Copy the test file before using it, and deleting it afterwards (to prevent change of modify-date').

--- bitrepository-core/src/test/java/org/bitrepository/protocol/utils/ConfigLoaderTest.java
+++ bitrepository-core/src/test/java/org/bitrepository/protocol/utils/ConfigLoaderTest.java
@@ -2,20 +2,34 @@ package org.bitrepository.protocol.utils;
 
 import java.io.File;
 
+import org.bitrepository.common.utils.FileUtils;
 import org.jaccept.structure.ExtendedTestCase;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ConfigLoaderTest extends ExtendedTestCase {
     
+    String GOOD_FILE_PATH = "logback-test.xml";
+    
+    @BeforeMethod (alwaysRun = true)
+    public void setup() {
+        FileUtils.copyFile(new File("src/test/resources/logback-test.xml"), new File(GOOD_FILE_PATH));
+    }
+    
+    @AfterMethod (alwaysRun = true)
+    public void teardown() {
+        FileUtils.delete(new File(GOOD_FILE_PATH));
+    }
+        
     @Test(groups = { "regressiontest" })
     public void testLoadingConfig() throws Exception {
         addDescription("Test the loading of a configuration file for the config loader.");
         addStep("Setup variables", "");
         String badFilePath = "iDoNotExist.xml";
         Assert.assertFalse(new File(badFilePath).exists());
-        String goodFilePath = "src/test/resources/logback-test.xml";
-        Assert.assertTrue(new File(goodFilePath).exists());
+        Assert.assertTrue(new File(GOOD_FILE_PATH).exists());
         
         addStep("Test with a invalid file path", "Should throw an exception");
         try {
@@ -26,12 +40,12 @@ public class ConfigLoaderTest extends ExtendedTestCase {
         }
         
         addStep("Test when the file is unreadable", "Should throw an exception");
-        File goodFile = new File(goodFilePath);
+        File goodFile = new File(GOOD_FILE_PATH);
         try {
             goodFile.setReadable(false);
             
             try {
-                new LogbackConfigLoader(goodFilePath);
+                new LogbackConfigLoader(GOOD_FILE_PATH);
                 Assert.fail("Should throw an exception");
             } catch (IllegalArgumentException e) {
                 // expected
@@ -43,7 +57,7 @@ public class ConfigLoaderTest extends ExtendedTestCase {
         }
         
         addStep("success case", "Should not throw an exception");
-        new LogbackConfigLoader(goodFilePath);        
+        new LogbackConfigLoader(GOOD_FILE_PATH);        
     }
     
 }
