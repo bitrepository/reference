90830d80e16a17ed2551fd780da459b60b4c20e3
ktc <ktc@statsbiblioteket.dk>
2012-07-13 11:33:55 +0200

    Add class and exception for protocol version support handling Update .gitignore

--- bitrepository-core/src/main/java/org/bitrepository/protocol/InvalidMessageVersionException.java
+++ bitrepository-core/src/main/java/org/bitrepository/protocol/InvalidMessageVersionException.java
@@ -1,5 +1,20 @@
 package org.bitrepository.protocol;
 
+import org.bitrepository.bitrepositoryelements.ResponseCode;
+import org.bitrepository.bitrepositoryelements.ResponseInfo;
+
 public class InvalidMessageVersionException extends RuntimeException {
 
+    private ResponseInfo response;
+    
+    public InvalidMessageVersionException(ResponseCode responseCode, String responseText) {
+        response = new ResponseInfo();
+        response.setResponseCode(responseCode);
+        response.setResponseText(responseText);
+    }
+    
+    public ResponseInfo getResponseInfo() {
+        return response;
+    }
+    
 }
--- bitrepository-core/src/main/java/org/bitrepository/protocol/MessageVersionValidator.java
+++ bitrepository-core/src/main/java/org/bitrepository/protocol/MessageVersionValidator.java
@@ -2,28 +2,32 @@ package org.bitrepository.protocol;
 
 import org.bitrepository.bitrepositoryelements.ResponseCode;
 import org.bitrepository.bitrepositorymessages.Message;
-
-
+import org.bitrepository.protocolversiondefinition.ProtocolVersionDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Class to handle message version validation for implementation of versioning support.  
  */
 public class MessageVersionValidator {
 
+    private static final Logger log = LoggerFactory.getLogger(MessageVersionValidator.class);
+    private static final ProtocolVersionDefinition protocolVersion = new ProtocolVersionDefinition();
+    
     private MessageVersionValidator() {}
     
     public static void validateMessageVersion(Message message) throws InvalidMessageVersionException {
-        /*if(message.getMinVersion().compareTo(context.protocolVersion) > 0) {
-            log.info("The Request minimum version is LARGER than this pillars version = " + context.protocolVersion);
+        if(message.getMinVersion().compareTo(protocolVersion.getVersion()) > 0) {
+            log.info("The Request minimum version is LARGER than this components version = " + protocolVersion.getVersion());
             throw new InvalidMessageVersionException(ResponseCode.REQUEST_NOT_UNDERSTOOD_FAILURE, 
-                    "The requested minimum version is too large, pillar version: "+ context.protocolVersion, 
-                    "Received message containing a message with an invalid message version.");
+                    "The requested minimum version is too large, components version: " + protocolVersion.getVersion());
         }
-        if(version != null && version.compareTo(context.protocolMinVersion) < 0) {
-            log.info("The Request version is SMALLER than this pillars minimum version = " + context.protocolMinVersion);
+        if(message.getVersion() != null && message.getVersion().compareTo(protocolVersion.getMinVersion()) < 0) {
+            log.info("The Request version is SMALLER than this components minimum version = " +
+                    protocolVersion.getMinVersion());
             throw new InvalidMessageVersionException(ResponseCode.REQUEST_NOT_UNDERSTOOD_FAILURE,
-                    "The requested version is smaller than the pillars minimum version, pillar minVersion: " +
-            context.protocolMinVersion, "Received message containing a message with an invalid message version.");
-        }*/
+                    "The requested version is smaller than the components minimum version, pillar minVersion: " +
+                    protocolVersion.getMinVersion());
+        }
     }
 }
