32261ff299b435c04182e53f87ffe61703e67a51
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2012-08-01 09:41:31 +0200

    BITMAG-662 Migrate reference pillar test to PillarAcceptaneTest: Preparing for merge.

--- bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/integration/PerformingPutFileIT.java
+++ bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/integration/PerformingPutFileIT.java
@@ -1,16 +1,32 @@
 package org.bitrepository.pillar.integration;
 
+import org.bitrepository.bitrepositorymessages.IdentifyPillarsForPutFileRequest;
+import org.bitrepository.bitrepositorymessages.IdentifyPillarsForPutFileResponse;
 import org.bitrepository.pillar.messagefactories.PutFileMessageFactory;
 import org.testng.annotations.BeforeMethod;
 
 public class PerformingPutFileIT extends PillarIntegrationTest {
     protected PutFileMessageFactory msgFactory;
 
-
     @BeforeMethod(alwaysRun=true)
     public void initialiseReferenceTest() throws Exception {
         msgFactory = new PutFileMessageFactory(componentSettings, getComponentID(), pillarDestinationId);
     }
 
+    protected String identifyPillarDestinationForPut(String pillarID) {s
+        IdentifyPillarsForPutFileRequest identifyRequest = msgFactory.createIdentifyPillarsForPutFileRequest(
+            DEFAULT_FILE_ID, 0L);
+        messageBus.sendMessage(identifyRequest);
 
+        addStep("Looking up " + pillarID + "s destination for put responses by sending a putFile identification " +
+                "and selecting the response with the test pillarID ",
+                "The pillar under test should make a response.");
+        while (true) {
+            IdentifyPillarsForPutFileResponse receivedIdentifyResponse = clientTopic.waitForMessage(
+                IdentifyPillarsForPutFileResponse.class);
+            if (receivedIdentifyResponse.getPillarID().equals(pillarID)) {
+                return receivedIdentifyResponse.getReplyTo();
+            }
+        }
+    }
 }
