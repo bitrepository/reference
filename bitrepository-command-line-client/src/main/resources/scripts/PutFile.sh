6f1705e695915be0851a10c0db5cd394e5e89419
jolf <jolf@kb.dk>
2012-09-04 15:38:32 +0200

    Followup on CR-BITMAG-167

--- bitrepository-command-line-client/src/main/resources/scripts/PutFile.sh
+++ bitrepository-command-line-client/src/main/resources/scripts/PutFile.sh
@@ -15,6 +15,3 @@ JAVA_OPTS="-classpath conf:lib/*"
 PUT_FILE="org.bitrepository.commandline.PutFile"
 
 exec $JAVA $JAVA_OPTS $PUT_FILE -s$CONFDIR -k$KEYFILE $* 
-
-
-#java -cp lib/activemq-core-5.6.0.jar:lib/bitrepository-access-client-0.19-SNAPSHOT.jar:lib/bitrepository-client-0.19-SNAPSHOT.jar:lib/bitrepository-collection-settings-0.9.jar:lib/bitrepository-command-line-0.19-SNAPSHOT.jar:lib/bitrepository-core-0.19-SNAPSHOT.jar:lib/bitrepository-message-xml-19.jar:lib/bitrepository-modifying-client-0.19-SNAPSHOT.jar:lib/commons-cli-20040117.000000.jar:lib/commons-io-2.4.jar:lib/commons-lang-2.6.jar:lib/jaxb2-basics-runtime-0.6.4.jar:lib/slf4j-api-1.6.5.jar:lib/bcmail-jdk16-1.46.jar:lib/bcprov-jdk16-1.46.jar:lib/geronimo-j2ee-management_1.1_spec-1.0.1.jar:lib/geronimo-jms_1.1_spec-1.1.1.jar org.bitrepository.commandline.PutFile -sconf -ktest.txt -ftest.txt -cd41d8cd98f00b204e9800998ecf8427e
