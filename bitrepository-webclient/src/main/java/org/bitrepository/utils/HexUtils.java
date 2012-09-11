2e4af2ed6d9390f4f1ad0785931e221df1f0c4fb
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2012-09-11 13:27:44 +0200

    BITMAG-719 - The HexUtils class should not log anything: Fixed

--- bitrepository-webclient/src/main/java/org/bitrepository/utils/HexUtils.java
+++ bitrepository-webclient/src/main/java/org/bitrepository/utils/HexUtils.java
@@ -21,12 +21,7 @@
  */
 package org.bitrepository.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class HexUtils {
-    private static final Logger log = LoggerFactory.getLogger(HexUtils.class);
-    
     public static String byteArrayToString(byte[] data) {
         StringBuffer sb = new StringBuffer(data.length * 2);
         for (int i = 0; i < data.length; i++){
@@ -36,19 +31,16 @@ public class HexUtils {
           }
           sb.append(Integer.toHexString(v));
         }
-        log.info("Converted byte array to string, string: " + sb.toString());
         return sb.toString();    
     }
     
     public static byte[] stringToByteArray(String hexString) {
         int len = hexString.length();
-        log.info("Trying to convert '" + hexString + "' to byte array");
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
                                  + Character.digit(hexString.charAt(i+1), 16));
         }
-        log.info("Converted string to byte array, string: " + hexString);
         return data;
     }
 }
