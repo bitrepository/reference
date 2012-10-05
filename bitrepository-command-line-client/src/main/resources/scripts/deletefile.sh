1ea96cc5eb02387919c74c9becd5549b6fa652a1
jolf <jolf@kb.dk>
2012-10-05 11:59:12 +0200

    Followup on CR-BITMAG-190

--- bitrepository-command-line-client/src/main/resources/scripts/deletefile.sh
+++ bitrepository-command-line-client/src/main/resources/scripts/deletefile.sh
@@ -26,4 +26,3 @@ source init.sh
 DELETE_FILE="org.bitrepository.commandline.DeleteFile"
 
 exec $JAVA $JAVA_OPTS $DELETE_FILE -s$CONFDIR -k$KEYFILE $* 
-
--- bitrepository-command-line-client/src/main/resources/scripts/getfile.sh
+++ bitrepository-command-line-client/src/main/resources/scripts/getfile.sh
@@ -26,5 +26,3 @@ source init.sh
 GET_FILE="org.bitrepository.commandline.GetFile"
 
 exec $JAVA $JAVA_OPTS $GET_FILE -s$CONFDIR -k$KEYFILE $* 
-
-
