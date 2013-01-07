5f43372fde5df22c22218042797211612449f010
ktc <ktc@statsbiblioteket.dk>
2013-01-07 14:23:04 +0100

    Add tests for non-standard checksum (SHA1 -/+ salt, MD5 + salt)

--- bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/integration/func/getchecksums/GetChecksumTest.java
+++ bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/integration/func/getchecksums/GetChecksumTest.java
@@ -26,6 +26,7 @@ import org.bitrepository.bitrepositoryelements.ChecksumDataForChecksumSpecTYPE;
 import org.bitrepository.bitrepositoryelements.ChecksumSpecTYPE;
 import org.bitrepository.bitrepositoryelements.ChecksumType;
 import org.bitrepository.client.exceptions.NegativeResponseException;
+import org.bitrepository.common.utils.Base16Utils;
 import org.bitrepository.pillar.integration.func.PillarFunctionTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -49,4 +50,61 @@ public class GetChecksumTest extends PillarFunctionTest {
             "Not implemented");
         // ToDo implement this
     }
+    
+    @Test ( groups = {"pillar-integration-test"} )
+    public void testSHA1Checksums() throws NegativeResponseException {
+        addDescription("Test the pillar support for SHA1 type checksums");
+        addReference("Not implemented");
+        pillarFileManager.ensureNumberOfFilesOnPillar(2, testMethodName);
+
+        addStep("Request SHA1 checksums for all files on the pillar",
+            "A list (at least 2 long) of SHA1 checksums should be returned.");
+        ChecksumSpecTYPE checksumSpec = new ChecksumSpecTYPE();
+        checksumSpec.setChecksumType(ChecksumType.SHA1);
+        List<ChecksumDataForChecksumSpecTYPE> checksums = pillarFileManager.getChecksums(checksumSpec, null);
+        Assert.assertTrue(checksums.size() >= 2, "The length of the returned checksums were less that 2");
+
+        addStep("Retrieve the first two files and verify that the checksums are correct",
+            "Not implemented");
+        // ToDo implement this
+    }
+
+    @Test ( groups = {"pillar-integration-test"} )
+    public void testMD5SaltChecksums() throws NegativeResponseException {
+        addDescription("Test the pillar support for MD5 type checksums with a salt");
+        addReference("Not implemented");
+        pillarFileManager.ensureNumberOfFilesOnPillar(2, testMethodName);
+
+        addStep("Request salted MD5 checksums for all files on the pillar",
+            "A list (at least 2 long) of MD5 checksums should be returned.");
+        ChecksumSpecTYPE checksumSpec = new ChecksumSpecTYPE();
+        checksumSpec.setChecksumType(ChecksumType.MD5);
+        checksumSpec.setChecksumSalt(Base16Utils.encodeBase16("abab"));
+        List<ChecksumDataForChecksumSpecTYPE> checksums = pillarFileManager.getChecksums(checksumSpec, null);
+        Assert.assertTrue(checksums.size() >= 2, "The length of the returned checksums were less that 2");
+
+        addStep("Retrieve the first two files and verify that the checksums are correct",
+            "Not implemented");
+        // ToDo implement this
+    }
+    
+    @Test ( groups = {"pillar-integration-test"} )
+    public void testSHA1SaltChecksums() throws NegativeResponseException {
+        addDescription("Test the pillar support for SHA1 type checksums with a salt");
+        addReference("Not implemented");
+        pillarFileManager.ensureNumberOfFilesOnPillar(2, testMethodName);
+
+        addStep("Request salted SHA1 checksums for all files on the pillar",
+            "A list (at least 2 long) of SHA1 checksums should be returned.");
+        ChecksumSpecTYPE checksumSpec = new ChecksumSpecTYPE();
+        checksumSpec.setChecksumType(ChecksumType.SHA1);
+        checksumSpec.setChecksumSalt(Base16Utils.encodeBase16("abab"));
+        List<ChecksumDataForChecksumSpecTYPE> checksums = pillarFileManager.getChecksums(checksumSpec, null);
+        Assert.assertTrue(checksums.size() >= 2, "The length of the returned checksums were less that 2");
+
+        addStep("Retrieve the first two files and verify that the checksums are correct",
+            "Not implemented");
+        // ToDo implement this
+    }
+    
 }
