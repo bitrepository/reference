7506aa06cbc01f8aab22970fdcf90fc2204dba55
ktc <ktc@statsbiblioteket.dk>
2013-02-07 14:26:35 +0100

    fix imports after merge mess....

--- bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/web/RestIntegrityService.java
+++ bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/web/RestIntegrityService.java
@@ -28,6 +28,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
