9953f7f8f23ab25bee5d6676b9802256b0f4cc5b
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2013-06-18 12:57:41 +0200

    Tuning quickstart

--- bitrepository-integration/src/main/resources/quickstart/scripts/quickstart.sh
+++ bitrepository-integration/src/main/resources/quickstart/scripts/quickstart.sh
@@ -26,130 +26,59 @@
 #Make sure we are in the scripts folder
 cd $(dirname $(perl -e "use Cwd 'abs_path';print abs_path('$0');"))
 #Go back to the quickstart "root dir"
-cd ..
 quickstartdir=$(pwd)
 
-#Find the configration directory
-cd conf/
-configdir=$(pwd)
-cd $quickstartdir
+#
+# Function that starts the quickstart components
+#
+do_start() {
+    export CATALINA_OPTS="-Xms256m -Xmx1028m -XX:PermSize=256m -XX:MaxPermSize=256m"
+    ${quickstartdir}/reference1pillar/bin/reference-pillar.sh start
+    ${quickstartdir}/reference2pillar/bin/reference-pillar.sh start
+    ${quickstartdir}/checksumpillar/bin/checksum-pillar.sh start
+    ${quickstartdir}/tomcat/bin/catalina.sh start
+}
 
-#Make a directory for log files
-if [ ! -d "logs" ]; then 
-	mkdir logs
-fi
+#
+# Function that stops the quickstart components
+#
+do_stop() {
+    ${quickstartdir}/tomcat/bin/catalina.sh stop -force
+    ${quickstartdir}/reference1pillar/bin/reference-pillar.sh stop
+    ${quickstartdir}/reference2pillar/bin/reference-pillar.sh stop
+    ${quickstartdir}/checksumpillar/bin/checksum-pillar.sh stop
+}
 
-#Make a directory for file storage
-if [ ! -d "var" ]; then
-	mkdir var
-fi
-cd var 
-if [ ! -d "checksumpillar" ]; then
-	mkdir checksumpillar
-fi
-if [ ! -d "reference1pillar" ]; then
-	mkdir reference1pillar
-fi
-if [ ! -d "reference2pillar" ]; then
-	mkdir reference2pillar
-fi
-cd $quickstartdir
+#
+# Function that lists the status of the quickstart components
+#
+do_status() {
+    ${quickstartdir}/reference1pillar/bin/reference-pillar.sh status
+    ${quickstartdir}/reference2pillar/bin/reference-pillar.sh status
+    ${quickstartdir}/checksumpillar/bin/checksum-pillar.sh status
+    ${quickstartdir}/tomcat/bin/catalina.sh status
+}
 
-#Setup configuration files, this includes:
-# - Create symlinks for CollectionSettings files in relevant configuration folders
-# - Setup paths to configs in relevant config files 
-#for directory in $(ls -l conf | grep "^d" | cut -d " " -f10) 
-for directory in $(find conf -maxdepth 1 -mindepth 1 -type d)
-do
-        cd "$directory"
-        ln -sf ../RepositorySettings.xml .
-        sed -i '' s%\<\!--foobarpattern--\>%$quickstartdir/% ReferenceSettings.xml > /dev/null
-        sed -i '' s%\$\{user.home\}%$quickstartdir% logback.xml
-        cd $quickstartdir
-done
+case "$1" in
+  start)
+	do_start
+	;;
+  stop)
+	do_stop
+	;;
+  status)
+	do_status
+	;;
+  restart|force-reload)
+	do_stop
+    do_start
+	;;
+  *)
+	echo "Usage: $SCRIPTNAME {start|stop|status|restart}" >&2
+	exit 4
+	;;
+esac
 
-sed -i '' s%eventsfile%$quickstartdir/logs/webclient-events% $configdir/webclient/webclient.properties
-
-for file in $(find tomcat-services -iname '*.xml')
-do
-	sed -i '' s%\${user.home}%$quickstartdir% $file
-done
-
-refpillartarfile=$(ls bitrepository-reference-pillar*.tar.gz)
-if [ ! -z $refpillartarfile ]; then
-	tar xvf $refpillartarfile > /dev/null
-	rm $refpillartarfile
-fi
-
-refpillardistdir=$(ls -t | grep bitrepository-reference-pillar-.* | head -1)
-if [ ! -d "checksumpillar" ]; then 
-	mkdir "checksumpillar"
-	cp -r $refpillardistdir/lib checksumpillar/.
-	cp -r $refpillardistdir/bin checksumpillar/.
-	ln -s ../conf/checksumpillar checksumpillar/conf
-fi
-cd checksumpillar/bin
-./checksum-pillar.sh start
-cd $quickstartdir
-
-if [ ! -d "reference1pillar" ]; then
-	mkdir "reference1pillar" 
-	cp -r $refpillardistdir/lib reference1pillar/.
-	cp -r $refpillardistdir/bin reference1pillar/.
-	ln -s ../conf/reference1pillar reference1pillar/conf
-fi
-cd reference1pillar/bin
-./reference-pillar.sh start
-cd $quickstartdir
-
-if [ ! -d "reference2pillar" ]; then
-        mkdir "reference2pillar"
-        cp -r $refpillardistdir/lib reference2pillar/.
-        cp -r $refpillardistdir/bin reference2pillar/.
-        ln -s ../conf/reference2pillar reference2pillar/conf
-fi
-cd reference2pillar/bin
-./reference-pillar.sh start
-cd $quickstartdir
-
-commandlinetarfile=$(ls bitrepository-command-line-*.tar.gz)
-if [ ! -z $commandlinetarfile ]; then
-        tar xvf $commandlinetarfile > /dev/null
-        rm $commandlinetarfile
-fi
-
-commandlinedistdir=$(ls -t | grep bitrepository-command-line-* | head -1)
-if [ ! -d "commandline" ]; then
-    mv $commandlinedistdir "commandline"
-    cd commandline
-    rm -rf conf
-    ln -s ../conf/commandline conf
-    ln -s conf/logback.xml
-fi
-cd $quickstartdir
-
-#Fetch, unpack, setup Apache Tomcat server
-if [ -d "tomcat" ]; then
-	rm -rf tomcat
-fi
-curl http://ftp.download-by.net/apache/tomcat/tomcat-6/v6.0.37/bin/apache-tomcat-6.0.37.tar.gz > tomcat.tar.gz
-tar xfz tomcat.tar.gz
-mv apache-tomcat-* tomcat
-
-cd tomcat/conf
-if [ ! -d Catalina ]; then
-        echo "Catalina dir is missing"
-        mkdir Catalina
-        cd Catalina
-        mkdir localhost
-fi
-cd $quickstartdir
-
-for app in bitrepository-webclient.xml bitrepository-alarm-service.xml bitrepository-integrity-service.xml bitrepository-audit-trail-service.xml  bitrepository-monitoring-service.xml ; do
-	ln -sf $quickstartdir/tomcat-services/$app $quickstartdir/tomcat/conf/Catalina/localhost/$app
-done
-
-./tomcat/bin/catalina.sh start
 
 
 
