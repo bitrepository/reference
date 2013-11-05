fdf72c428bd19f675b2f501b151e7988cadfaba7
jolf <jolf@kb.dk>
2013-11-05 13:44:17 +0100

    BITMAG-1011: Fixed bad migration script

--- bitrepository-alarm-service/src/main/resources/sql/derby/alarmServiceDB1to2migration.sql
+++ bitrepository-alarm-service/src/main/resources/sql/derby/alarmServiceDB1to2migration.sql
@@ -28,10 +28,8 @@ UPDATE tableversions SET version=2 WHERE tablename='alarm';
 
 
 -- Alter alarm table. 
-ALTER TABLE alarm (
-    ADD COLUMN collection_id VARCHAR(255),
-    ADD FOREIGN KEY (component_guid) REFERENCES component(component_guid)
-);
+ALTER TABLE alarm ADD COLUMN collection_id VARCHAR(255);
+ALTER TABLE alarm ADD FOREIGN KEY (component_guid) REFERENCES component(component_guid);
 
 CREATE INDEX collectionidindex ON alarm ( collection_id );
 
