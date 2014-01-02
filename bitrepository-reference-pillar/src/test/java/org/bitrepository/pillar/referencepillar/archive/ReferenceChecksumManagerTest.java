de3d8a32106d0436daa1e996a9ac139d282e44dc
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2014-01-02 17:04:08 +0100

    BITMAG-801 Fix ReferenceChecksumManagerTest.testRecalculationWithMockCache: Changed test case to real mocked test.

--- bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/referencepillar/archive/ReferenceChecksumManagerTest.java
+++ bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/referencepillar/archive/ReferenceChecksumManagerTest.java
@@ -5,150 +5,132 @@
  * Copyright (C) 2010 - 2012 The State and University Library, The Royal Library and The State Archives, Denmark
  * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 2.1 of the 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU General Lesser Public 
+ *
+ * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-2.1.html>.
  * #L%
  */
 package org.bitrepository.pillar.referencepillar.archive;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.Date;
+        import java.io.ByteArrayInputStream;
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.util.Arrays;
+        import java.util.Collection;
+        import java.util.Date;
+        import java.util.LinkedList;
 
-import org.bitrepository.common.filestore.DefaultFileInfo;
-import org.bitrepository.common.utils.Base16Utils;
-import org.bitrepository.common.utils.CalendarUtils;
-import org.bitrepository.common.utils.ChecksumUtils;
-import org.bitrepository.common.utils.FileUtils;
-import org.bitrepository.pillar.DefaultFixturePillarTest;
-import org.bitrepository.pillar.cache.ChecksumDAO;
-import org.bitrepository.pillar.cache.ChecksumDatabaseManager;
-import org.bitrepository.pillar.cache.ChecksumStore;
-import org.bitrepository.pillar.cache.MemoryCacheMock;
-import org.bitrepository.pillar.cache.database.ExtractedChecksumResultSet;
-import org.bitrepository.pillar.common.ChecksumDatabaseCreator;
-import org.bitrepository.service.AlarmDispatcher;
-import org.bitrepository.service.database.DerbyDatabaseDestroyer;
-import org.bitrepository.settings.referencesettings.DatabaseSpecifics;
-import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+        import org.bitrepository.bitrepositoryelements.ChecksumSpecTYPE;
+        import org.bitrepository.common.filestore.DefaultFileInfo;
+        import org.bitrepository.common.filestore.FileInfo;
+        import org.bitrepository.common.filestore.FileStore;
+        import org.bitrepository.common.settings.Settings;
+        import org.bitrepository.common.settings.TestSettingsProvider;
+        import org.bitrepository.common.utils.*;
+        import org.bitrepository.pillar.DefaultFixturePillarTest;
+        import org.bitrepository.pillar.cache.*;
+        import org.bitrepository.pillar.cache.database.ExtractedChecksumResultSet;
+        import org.bitrepository.pillar.common.ChecksumDatabaseCreator;
+        import org.bitrepository.service.AlarmDispatcher;
+        import org.bitrepository.service.database.DerbyDatabaseDestroyer;
+        import org.bitrepository.settings.referencesettings.DatabaseSpecifics;
+        import org.jaccept.structure.ExtendedTestCase;
+        import org.testng.Assert;
+        import org.testng.annotations.BeforeMethod;
+        import org.testng.annotations.Test;
 
-public class ReferenceChecksumManagerTest extends DefaultFixturePillarTest {
-    private static final String TEST_FILE = "testFile"; 
-    private static final String TEST_CONTENT = "This is the initial content."; 
-    private static final String TEST_ADDED_CONTENT = "This is the newly added content to change the file."; 
+        import javax.xml.datatype.XMLGregorianCalendar;
+
+        import static org.mockito.Mockito.*;
+        import static org.testng.AssertJUnit.assertEquals;
+        import static org.testng.AssertJUnit.assertTrue;
+
+public class ReferenceChecksumManagerTest extends ExtendedTestCase {
+    private static final String DEFAULT_COLlECTION_ID = "defaultTestCollection";
+    private static final String COMPONENTID = "ChecksumManagerUnderTest";
+
+    private AlarmDispatcher alarmDispatcher;
+    private CollectionArchiveManager archiveManager;
+    private ChecksumStore cache;
+    private Settings settings = TestSettingsProvider.getSettings(COMPONENTID);
+    private ChecksumSpecTYPE defaultChecksumType;
 
     @BeforeMethod(alwaysRun=true)
     public void initialiseReferenceTest() throws Exception {
-        File fileDir = new File(settingsForCUT.getReferenceSettings().getPillarSettings().getCollectionDirs().get(0).getFileDirs().get(0));
-        if(fileDir.exists()) {
-            FileUtils.delete(fileDir);
-        }
+        alarmDispatcher = mock(AlarmDispatcher.class);
+        archiveManager = mock(CollectionArchiveManager.class);
+        cache = mock(ChecksumStore.class);
+        settings = TestSettingsProvider.getSettings("");
+        defaultChecksumType = ChecksumUtils.getDefault(settings);
     }
 
-    //@Test( groups = {"regressiontest", "pillartest"})
-    // Fails occasionally, see "BITMAG-801 - Fix ReferenceChecksumManagerTest.testRecalculationWithMockCache"
-    public void testRecalculationWithMockCache() throws Exception {
-        addDescription("Test the ability to recalculate the checksums automatically with a mock cache.");
-        CollectionArchiveManager archives = new CollectionArchiveManager(settingsForCUT);
-        ChecksumStore csCache = new MemoryCacheMock();
-        AlarmDispatcher alarmDispatcher = new AlarmDispatcher(settingsForCUT, messageBus);
-        ReferenceChecksumManager csManager =
-                new ReferenceChecksumManager(archives, csCache, alarmDispatcher, settingsForCUT);
+    @Test( groups = {"regressiontest"})
+    public void testEmptyArchive() {
+        addDescription("Test the ChecksumManager on a empty archive");
 
-        testRecalculatingChecksumsDueToChangedFile(csManager, archives);
+        addStep("Call the getEntries method where the cache and archive returns a empty list of files/checksums.",
+                "The call should be delegated to the checksum cache, and the result from here should nbe returned. ");
+        ReferenceChecksumManager checksumManagerUnderTestManager = new ReferenceChecksumManager(
+                archiveManager, cache, alarmDispatcher, settings);
+        when(cache.getAllFileIDs(DEFAULT_COLlECTION_ID)).thenReturn(new LinkedList<String>());
+        when(archiveManager.getAllFileIds(DEFAULT_COLlECTION_ID)).thenReturn(new LinkedList<String>());
+        XMLGregorianCalendar minTimeStamp = CalendarUtils.getXmlGregorianCalendar(new Date());
+        XMLGregorianCalendar maxTimeStamp = CalendarUtils.getXmlGregorianCalendar(new Date());
+        long maxNumberOfResults = 10;
+        ExtractedChecksumResultSet expectedCsResults = new ExtractedChecksumResultSet();
+        when(cache.getEntries(minTimeStamp, maxTimeStamp, maxNumberOfResults, DEFAULT_COLlECTION_ID)).thenReturn(
+                expectedCsResults);
+
+        ExtractedChecksumResultSet receivedCsResults = checksumManagerUnderTestManager.getEntries(
+                minTimeStamp, maxTimeStamp, maxNumberOfResults, DEFAULT_COLlECTION_ID, defaultChecksumType);
+        assertTrue(expectedCsResults == receivedCsResults);
     }
 
-    @Test( groups = {"regressiontest", "pillartest"})
-    public void testRecalculationWithRealCache() throws Exception {
-        addDescription("Test the ability to recalculate the checksums automatically with a database cache.");
+    @Test( groups = {"regressiontest"})
+    public void testChecksumRecalculation() {
+        addDescription("Verifies that the checksum is recalculated if the files has changed on disk, eg. the modified" +
+                "time stamp of the files has changed.");
 
-        DatabaseSpecifics checksumDB =
-                settingsForCUT.getReferenceSettings().getPillarSettings().getChecksumDatabase();
-        DerbyDatabaseDestroyer.deleteDatabase(checksumDB);
-
-        ChecksumDatabaseCreator checksumDatabaseCreator = new ChecksumDatabaseCreator();
-        checksumDatabaseCreator.createChecksumDatabase(settingsForCUT, null);
-
-        CollectionArchiveManager archives = new CollectionArchiveManager(settingsForCUT);
-        ChecksumStore csCache = new ChecksumDAO(new ChecksumDatabaseManager(settingsForCUT));
-        ReferenceChecksumManager csManager = new ReferenceChecksumManager(archives, csCache, 
-                new AlarmDispatcher(settingsForCUT, messageBus), settingsForCUT);
-
-        testRecalculatingChecksumsDueToChangedFile(csManager, archives);
-    }
-
-    private void testRecalculatingChecksumsDueToChangedFile(ReferenceChecksumManager csManager, CollectionArchiveManager archives) throws Exception {
-        Date initialDate = new Date();
-        ExtractedChecksumResultSet initialCsResults = csManager.getEntries(null, null, null, collectionID, 
-                ChecksumUtils.getDefault(settingsForCUT));
-        Assert.assertEquals(initialCsResults.getEntries().size(), 0);
-
-        synchronized(this) {
-            addStep("The file system cannot handle timestamps in millis.", "One second wait, before inserting a file.");
-            this.wait(1000);
-        }
-
-        addStep("Put a file into the archive", "The checksum of the file is extracted through the checksum entries");
-        archives.downloadFileForValidation(TEST_FILE, collectionID, new ByteArrayInputStream(TEST_CONTENT.getBytes()));
-        archives.moveToArchive(TEST_FILE, collectionID);
-        ExtractedChecksumResultSet csResults = csManager.getEntries(null, null, null, collectionID, 
-                ChecksumUtils.getDefault(settingsForCUT));
-        Assert.assertEquals(csResults.getEntries().size(), 1);
-
-        addStep("Validate the dates", "Calculation time should be between the initial test time and now.");
-        Date beforeChange = new Date();
-        Assert.assertTrue(CalendarUtils.convertFromXMLGregorianCalendar(
-                csResults.getEntries().get(0).getCalculationTimestamp()).getTime() > initialDate.getTime());
-        Assert.assertTrue(CalendarUtils.convertFromXMLGregorianCalendar(
-                csResults.getEntries().get(0).getCalculationTimestamp()).getTime() < beforeChange.getTime());
-        Assert.assertTrue(archives.getFileInfo(TEST_FILE, collectionID).getMdate() >= initialDate.getTime()); 
-        Assert.assertTrue(archives.getFileInfo(TEST_FILE, collectionID).getMdate() <= beforeChange.getTime());
-
-        synchronized(this) {
-            addStep("The file system cannot handle timestamps in millis.", "One second wait, before changing the file.");
-            this.wait(1000);
-        }
+        addStep("Call the getEntries method and return a checksum timestamp from the cache older than the archived file " +
+                "modification date.", "A call should be made to the recalculateChecksum for the concrete fileID.");
+        TestReferenceChecksumManager checksumManagerUnderTestManager = new TestReferenceChecksumManager(
+                archiveManager, cache, alarmDispatcher, settings);
 
         addStep("Change the file", "Should be recalculated and thus have a newer timestamp");
-        DefaultFileInfo dfi = (DefaultFileInfo) archives.getFileInfo(TEST_FILE, collectionID);
-        FileOutputStream out = new FileOutputStream(dfi.getFile(), true);
-        out.write("\n".getBytes());
-        out.write(TEST_ADDED_CONTENT.getBytes());
-        out.flush();
-        out.close();
-
-        ExtractedChecksumResultSet changedCsResults = csManager.getEntries(null, null, null, collectionID, 
-                ChecksumUtils.getDefault(settingsForCUT));
-        Assert.assertEquals(changedCsResults.getEntries().size(), 1);
-
-        addStep("Validate the dates", "New calculation time should be between the initial test time and now.");
-        Date afterChange = new Date();
-        Assert.assertTrue(CalendarUtils.convertFromXMLGregorianCalendar(
-                changedCsResults.getEntries().get(0).getCalculationTimestamp()).getTime() > initialDate.getTime());
-        Assert.assertTrue(CalendarUtils.convertFromXMLGregorianCalendar(
-                changedCsResults.getEntries().get(0).getCalculationTimestamp()).getTime() > beforeChange.getTime());
-        Assert.assertTrue(CalendarUtils.convertFromXMLGregorianCalendar(
-                changedCsResults.getEntries().get(0).getCalculationTimestamp()).getTime() <= afterChange.getTime());
-        Assert.assertTrue(archives.getFileInfo(TEST_FILE, collectionID).getMdate() >= initialDate.getTime());
-        Assert.assertTrue(archives.getFileInfo(TEST_FILE, collectionID).getMdate() >= beforeChange.getTime());
-        Assert.assertTrue(archives.getFileInfo(TEST_FILE, collectionID).getMdate() <= afterChange.getTime());
-
-        addStep("Test the checksum of before and after the change", "Not identical");
-        String csBefore = Base16Utils.decodeBase16(csResults.getEntries().get(0).getChecksumValue());
-        String csAfter = Base16Utils.decodeBase16(changedCsResults.getEntries().get(0).getChecksumValue());
-        Assert.assertFalse(csBefore.equals(csAfter));
+        String fileId = "testFile";
+        when(archiveManager.getAllFileIds(DEFAULT_COLlECTION_ID)).thenReturn(Arrays.asList(new String[] {fileId}));
+        when(cache.hasFile(fileId, DEFAULT_COLlECTION_ID)).thenReturn(true);
+        ExtractedChecksumResultSet expectedCsResults = new ExtractedChecksumResultSet();
+        ChecksumEntry cs = new ChecksumEntry(fileId, "aa", new Date(System.currentTimeMillis()-10));
+        expectedCsResults.insertChecksumEntry(cs);
+        when(cache.getEntries(null, null, null, DEFAULT_COLlECTION_ID)).thenReturn(expectedCsResults);
+        when(cache.getCalculationDate(fileId, DEFAULT_COLlECTION_ID)).thenReturn(new Date());
+        FileInfo fileInfoMock = mock(FileInfo.class);
+        when(fileInfoMock.getMdate()).thenReturn(new Date().getTime());
+        when(archiveManager.getFileInfo(fileId,DEFAULT_COLlECTION_ID)).thenReturn(fileInfoMock);
+        checksumManagerUnderTestManager.getEntries(
+                null, null, null, DEFAULT_COLlECTION_ID, defaultChecksumType);
+        assertTrue(checksumManagerUnderTestManager.recalculateCheckSumCalled);
     }
+
+    class TestReferenceChecksumManager extends ReferenceChecksumManager {
+        boolean recalculateCheckSumCalled = false;
+        public TestReferenceChecksumManager(FileStore archives, ChecksumStore cache, AlarmDispatcher alarmDispatcher, Settings settings) {
+            super(archives, cache, alarmDispatcher, settings);
+        }
+
+        @Override
+        public void recalculateChecksum(String fileId, String collectionId) {
+            recalculateCheckSumCalled = true;
+        }
+    };
 }
