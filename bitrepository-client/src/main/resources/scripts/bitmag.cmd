76bcbfaa9ab46766c511f65447bd6cf68c81bbac
ktc <ktc@statsbiblioteket.dk>
2014-04-24 09:44:49 +0200

    introduce new unified scripts for client, one for *nix based systems, one for windows based

---
+++ bitrepository-client/src/main/resources/scripts/bitmag.cmd
@@ -0,0 +1,34 @@
+rem @ECHO OFF
+
+set JAVA=c:\Program Files (x86)\Java\jre7\bin\java.exe
+
+set BASEDIR=%0%
+for %%x in ("%BASEDIR%") DO set BASEDIR=%%~dpx..
+set CONFDIR=%BASEDIR%\conf
+set KEYFILE=%CONFDIR%\mycert-key.pem
+set JAVA_OPTS=-classpath  "%BASEDIR%\lib\*"
+set JAVA_OPTS=%JAVA_OPTS% "-Dlogback.configurationFile=%CONFDIR%\logback.xml"
+set JAVA_OPTS=%JAVA_OPTS% "-DBASEDIR=%BASEDIR%"
+
+set CMD=help
+if /I x%1%==xdelete        set CMD=DeleteFile
+if /I x%1%==xget-checksums set CMD=GetChecksums
+if /I x%1%==xget-file      set CMD=GetFile
+if /I x%1%==xget-file-ids  set CMD=GetFileIDs
+if /I x%1%==xput-file      set CMD=PutFile -d
+if /I x%1%==xreplace-file  set CMD=ReplaceFile
+
+if not "%CMD%"=="help" goto run
+echo. usage: %0% CMD PARAMS
+echo.   CMD is one of
+echo.     delete
+echo.     get-checksums
+echo.     get-file
+echo.     get-file-ids
+echo.     put-file
+echo.     replace-file
+goto :eof
+:run
+set CMD=org.bitrepository.commandline.%CMD%
+shift
+"%JAVA%" %JAVA_OPTS% %CMD% "-k%KEYFILE%" "-s%CONFDIR%" %*%
---
+++ bitrepository-client/src/main/resources/scripts/bitmag.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+BASEDIR=$(dirname $(dirname $(perl -e "use Cwd 'abs_path';print abs_path('$0');")))
+CONFDIR="$BASEDIR/conf"
+KEYFILE="$CONFDIR/mycert-key.pem"
+JAVA_OPTS="-classpath  $BASEDIR/lib/*"
+JAVA_OPTS="$JAVA_OPTS -Dlogback.configurationFile=$CONFDIR/logback.xml"
+JAVA_OPTS="$JAVA_OPTS -DBASEDIR=$BASEDIR"
+
+case "$1" in
+    delete)		CMD=DeleteFile		;;
+    get-checksums)	CMD=GetChecksums	;;
+    get-file)		CMD=GetFile		;;
+    get-file-ids)	CMD=GetFileIDs		;;
+    put-file)		CMD=PutFile		;;
+    replace-file)	CMD=ReplaceFile		;;
+    *)
+	exec 1>&2
+	echo "usage: $0 CMD PARAMS"
+	echo "  CMD is one of"
+	echo "    delete"
+	echo "    get-checksums"
+	echo "    get-file"
+	echo "    get-file-ids"
+	echo "    put-file"
+	echo "    replace-file"
+	exit 1
+	;;
+esac
+CMD=org.bitrepository.commandline.$CMD
+shift
+exec java $JAVA_OPTS $CMD -k$KEYFILE -s$CONFDIR $*
