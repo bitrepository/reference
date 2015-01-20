2ab00952afe008f7c4c56c2203efe9ec2f68048c
ktc <ktc@statsbiblioteket.dk>
2015-01-20 14:55:45 +0100

    move monitoring service to new jersey 2.15 from old 1.18

---
+++ bitrepository-monitoring-service/src/main/java/org/bitrepository/monitoringservice/webservice/WebConfOption.java
@@ -0,0 +1,37 @@
+package org.bitrepository.monitoringservice.webservice;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class WebConfOption {
+
+    private String confOption;
+    private String confValue;
+    
+    public WebConfOption() {}
+    
+    public WebConfOption(String option, String value) {
+        this.confOption = option;
+        this.confValue = value;
+    }
+    
+    @XmlElement(name = "confOption")
+    public String getConfOption() {
+        return confOption;
+    }
+    
+    public void setConfOption(String confOption) {
+        this.confOption = confOption;
+    }
+    
+    @XmlElement(name = "confValue")
+    public String getConfValue() {
+        return confValue;
+    }
+    
+    public void setConfValue(String confValue) {
+        this.confValue = confValue;
+    }
+    
+    
+    
+}
---
+++ bitrepository-monitoring-service/src/main/java/org/bitrepository/monitoringservice/webservice/WebStatus.java
@@ -0,0 +1,67 @@
+package org.bitrepository.monitoringservice.webservice;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.bitrepository.common.utils.TimeUtils;
+import org.bitrepository.monitoringservice.status.ComponentStatus;
+
+public class WebStatus {
+
+    private String componentID;
+    private String status;
+    private String info;
+    private String timeStamp;
+    
+    public WebStatus() {}
+    
+    public WebStatus(String componentID, ComponentStatus status) {
+        this.componentID = componentID;
+        this.status = status.getStatus().toString();
+        this.info = status.getInfo();
+        XMLGregorianCalendar cal = status.getLastReply();
+        if(cal != null) {
+            timeStamp = TimeUtils.shortDate(cal);
+        } else {
+            timeStamp = "N/A";    
+        }
+    }
+    
+    @XmlElement(name = "componentID")
+    public String getComponentID() {
+        return componentID;
+    }
+    
+    public void setComponentID(String componentID) {
+        this.componentID = componentID;
+    }
+    
+    @XmlElement(name = "status")
+    public String getStatus() {
+        return status;
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    @XmlElement(name = "info")
+    public String getInfo() {
+        return info;
+    }
+    
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    
+    @XmlElement(name = "timeStamp")
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+    
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+    
+    
+}
