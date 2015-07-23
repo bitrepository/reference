9464e43330a7a7ae00889536827bb1f6015b413c
ktc <ktc@statsbiblioteket.dk>
2015-07-23 12:43:08 +0200

    prepare AlarmService for database specific DAO classes

---
+++ bitrepository-alarm-service/src/main/java/org/bitrepository/alarm/store/AlarmDAOFactory.java
@@ -0,0 +1,28 @@
+package org.bitrepository.alarm.store;
+
+import org.bitrepository.common.settings.Settings;
+import org.bitrepository.service.database.UnsupportedDatabaseTypeException;
+
+/**
+ * Factory class for obtaining the appropriate DAO for the specific database backend 
+ */
+public class AlarmDAOFactory {
+    
+    private static final String derbyDriver = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static final String postgressDriver = "org.postgresql.Driver";
+    
+    public static AlarmServiceDAO getAlarmServiceDAOInstance(Settings settings) {
+        AlarmDatabaseManager dm = new AlarmDatabaseManager(
+                settings.getReferenceSettings().getAlarmServiceSettings().getAlarmServiceDatabase());
+        String dbDriver = settings.getReferenceSettings().getAlarmServiceSettings().getAlarmServiceDatabase().getDriverClass();
+        if(dbDriver.equals(derbyDriver)) {
+            return new AlarmServiceDAO(dm);
+        } else if(dbDriver.equals(postgressDriver)) {
+            return new AlarmServiceDAO(dm);
+        } else {
+            throw new UnsupportedDatabaseTypeException("The database for driver: '" + dbDriver
+                    + "' is not supported, use '" + derbyDriver + "' or '" + postgressDriver + "'");
+        }
+    }
+    
+}
