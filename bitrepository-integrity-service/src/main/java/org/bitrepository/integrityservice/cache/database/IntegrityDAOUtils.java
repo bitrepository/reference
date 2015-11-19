d11c7eea29f7d4d4e34ba13adc730ed538d2b3a0
Jonas Lindberg Frellesen <jolf@kb.dk>
2015-11-19 14:21:59 +0100

    Added a lot of license missing headers (though they probably need update). Removed unused imports and an unused log, and bumped to version 1.8 of the license tool

--- bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/cache/database/IntegrityDAOUtils.java
+++ bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/cache/database/IntegrityDAOUtils.java
@@ -1,16 +1,33 @@
+/*
+ * #%L
+ * Bitrepository Integrity Service
+ * %%
+ * Copyright (C) 2010 - 2012 The State and University Library, The Royal Library and The State Archives, Denmark
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 2.1 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
 package org.bitrepository.integrityservice.cache.database;
 
 import java.util.List;
+
 import org.bitrepository.common.ArgumentValidator;
 import org.bitrepository.service.database.DBConnector;
 import org.bitrepository.service.database.DatabaseUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class IntegrityDAOUtils {
-    /** The log.*/
-    private Logger log = LoggerFactory.getLogger(getClass());
-    
     /** The connector to the database.*/
     protected final DBConnector dbConnector;
     
