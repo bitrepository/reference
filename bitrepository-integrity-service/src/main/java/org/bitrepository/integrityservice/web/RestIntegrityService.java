10b99c1d81fecc0f5cb18d9038820dc11eb16323
Ole V. Villumsen <olvi@kb.dk>
2022-07-26 12:55:59 +0200

    BITMAG-1142 Checksum age in GUI: underway

--- bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/web/RestIntegrityService.java
+++ bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/web/RestIntegrityService.java
@@ -250,7 +250,7 @@ public class RestIntegrityService {
                 PillarType pillarTypeObject = SettingsUtils.getPillarType(pillar);
                 String pillarType = pillarTypeObject != null ? pillarTypeObject.value() : null;
                 PillarCollectionStat emptyStat = new PillarCollectionStat(pillar, collectionID, pillarName, pillarType,
-                        0L, 0L, 0L, 0L, 0L, 0L, new Date(0), new Date(0));
+                        0L, 0L, 0L, 0L, 0L, 0L, "", "", new Date(0), new Date(0));
                 stats.put(pillar, emptyStat);
             }
         }
@@ -422,6 +422,8 @@ public class RestIntegrityService {
         jg.writeObjectField("checksumErrorCount", stat.getChecksumErrors());
         jg.writeObjectField("obsoleteChecksumsCount", stat.getObsoleteChecksums());
         jg.writeObjectField("missingChecksumsCount", stat.getMissingChecksums());
+        jg.writeObjectField("maxAgeForChecksums", stat.getMaxAgeForChecksums());
+        jg.writeObjectField("ageOfOldestChecksum", stat.getAgeOfOldestChecksum());
         jg.writeEndObject();
     }
 
