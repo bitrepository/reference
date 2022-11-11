254d89ad2b20640e8a4fb1a606d31d75bd783d78
bohlski <rbkr@kb.dk>
2022-11-11 14:19:31 +0100

    Clean js dashboard_components files

--- bitrepository-webclient/src/main/webapp/dashboard_components/collectionStatus.js
+++ bitrepository-webclient/src/main/webapp/dashboard_components/collectionStatus.js
@@ -1,108 +1,104 @@
+let collections = {};
+let readyForRefresh = false;
+let integrityServiceUrl;
+let myNameMapper;
+let updatePage;
+let updateLock;
 
-
-  var collections = new Object();
-  var readyForRefresh = false;
-  var integrityServiceUrl;
-  var myNameMapper;
-  var update_page;
-  var updateLock;
-
-  function setIntegrityServiceUrl(url) {
+function setIntegrityServiceUrl(url) {
     integrityServiceUrl = url;
-  }
+}
 
-  function setNameMapper(nameMapper) {
+function setNameMapper(nameMapper) {
     myNameMapper = nameMapper;
-  }
+}
 
-  function initiateCollectionStatus(collectionIDs, tableBody, updateInterval) {
-    for(var i = 0; i < collectionIDs.length; i++) {
+function initiateCollectionStatus(collectionIDs, tableBody, updateInterval) {
+    for (let i = 0; i < collectionIDs.length; i++) {
         collections[collectionIDs[i].collectionID] = {collectionID: collectionIDs[i].collectionID,
-                                         collectionName: collectionIDs[i].collectionName,
-                                         numFiles: '-',
-                                         latestIngest: "Fetching",
-                                         collectionSize: '-',
-                                         pillars: '-',
-                                         lastCheck: "Fetching",
-                                         numChecksumErrors: '-',
-                                         numMissingFiles: '-',
-                                         nextCheck: "Fetching"};
+            collectionName: collectionIDs[i].collectionName,
+            numFiles: '-',
+            latestIngest: "Fetching",
+            collectionSize: '-',
+            pillars: '-',
+            lastCheck: "Fetching",
+            numChecksumErrors: '-',
+            numMissingFiles: '-',
+            nextCheck: "Fetching"};
         $(tableBody).append(makeCollectionRow(collections[collectionIDs[i].collectionID]));
-      }
-      readyForRefresh = true;
-      initStatusUpdate(updateInterval);
-  }
-
-  function updateWorkflowStatus(collection) {
-    if(scheduleUpdate(collection, "status")) {
-      url = integrityServiceUrl + "/integrity/IntegrityService/getWorkflowSetup/?collectionID=" + collection;
-      var c = collection;
-      $.getJSON(url, {}, function(j) {
-        collections[c].lastCheck = j[0].lastRun;
-        collections[c].nextCheck = j[0].nextRun;
-      }).done(function(){updateCollectionRow(collections[c]);}).always(function() {finishUpdate(c, "status");});
     }
-  }
+    readyForRefresh = true;
+    initStatusUpdate(updateInterval);
+}
 
-  function updateWorkflowStatuses() {
-    if(readyForRefresh) {
-      for(c in collections) {
-        updateWorkflowStatus(c);
-      }
+function updateWorkflowStatus(collection) {
+    if (scheduleUpdate(collection, "status")) {
+        let url = integrityServiceUrl + "/integrity/IntegrityService/getWorkflowSetup/?collectionID=" + collection;
+        let c = collection;
+        $.getJSON(url, {}, function(j) {
+            collections[c].lastCheck = j[0].lastRun;
+            collections[c].nextCheck = j[0].nextRun;
+        }).done(function(){updateCollectionRow(collections[c]);}).always(function() {finishUpdate(c, "status");});
     }
-  }
+}
 
-  function updateCollectionInfo(collection) {
-    if(scheduleUpdate(collection, "info")) {
-      url = integrityServiceUrl + "/integrity/IntegrityService/getCollectionInformation/?collectionID=" + collection;
-      var c = collection;
-      $.getJSON(url, {}, function(j) {
-        collections[c].numFiles = j.numberOfFiles;
-        collections[c].collectionSize = j.collectionSize;
-        collections[c].latestIngest = j.lastIngest;
-      }).done(function(){updateCollectionRow(collections[c]);}).always(function() {finishUpdate(c, "info");});
-    }
-  }
-
-  function updateInfo() {
-    if(readyForRefresh) {
-      for(c in collections) {
-        updateCollectionInfo(c);
-      }
-    }
-  }
-
-  function updateCollectionStatistic(collection) {
-    if(scheduleUpdate(collection, "stats")) {
-      url = integrityServiceUrl + "/integrity/IntegrityService/getIntegrityStatus/?collectionID=" + collection;
-      var c = collection;
-      $.getJSON(url, {}, function(j) {
-        var checksumErrors = 0;
-        var missingFiles = 0;
-        var pillarCount = 0;
-        for(stat in j) {
-          pillarCount += 1;
-          checksumErrors += j[stat].checksumErrorCount;
-          missingFiles += j[stat].missingFilesCount;
+function updateWorkflowStatuses() {
+    if (readyForRefresh) {
+        for (let c in collections) {
+            updateWorkflowStatus(c);
         }
-        collections[c].pillars = pillarCount;
-        collections[c].numChecksumErrors = checksumErrors;
-        collections[c].numMissingFiles = missingFiles;
-      }).done(function(){updateCollectionRow(collections[c])}).always(function() {finishUpdate(c, "stats");});
     }
-  }
+}
 
-  function updateStatistics() {
-    if(readyForRefresh) {
-      for(c in collections) {
-        updateCollectionStatistic(c);
-      }
+function updateCollectionInfo(collection) {
+    if (scheduleUpdate(collection, "info")) {
+        let url = integrityServiceUrl + "/integrity/IntegrityService/getCollectionInformation/?collectionID=" + collection;
+        $.getJSON(url, {}, function(j) {
+            collections[collection].numFiles = j.numberOfFiles;
+            collections[collection].collectionSize = j.collectionSize;
+            collections[collection].latestIngest = j.lastIngest;
+        }).done(function(){updateCollectionRow(collections[c]);}).always(function() {finishUpdate(c, "info");});
     }
-  }
+}
 
-  function makeCollectionRow(collection) {
-    var id = collection.collectionID;
-    var html = "";
+function updateInfo() {
+    if (readyForRefresh) {
+        for (let c in collections) {
+            updateCollectionInfo(c);
+        }
+    }
+}
+
+function updateCollectionStatistic(collection) {
+    if (scheduleUpdate(collection, "stats")) {
+        let url = integrityServiceUrl + "/integrity/IntegrityService/getIntegrityStatus/?collectionID=" + collection;
+        $.getJSON(url, {}, function(j) {
+            let checksumErrors = 0;
+            let missingFiles = 0;
+            let pillarCount = 0;
+            for (let stat in j) {
+                pillarCount += 1;
+                checksumErrors += j[stat].checksumErrorCount;
+                missingFiles += j[stat].missingFilesCount;
+            }
+            collections[collection].pillars = pillarCount;
+            collections[collection].numChecksumErrors = checksumErrors;
+            collections[collection].numMissingFiles = missingFiles;
+        }).done(function(){updateCollectionRow(collections[c])}).always(function() {finishUpdate(c, "stats");});
+    }
+}
+
+function updateStatistics() {
+    if (readyForRefresh) {
+        for (let c in collections) {
+            updateCollectionStatistic(c);
+        }
+    }
+}
+
+function makeCollectionRow(collection) {
+    let id = collection.collectionID;
+    let html = "";
     html += "<tr id=\"" + id + "-row\">";
     html += "<td id=\""+ id + "-name\" class=\"collectionName\">" + id + "</div></td>";
     html += "<td id=\""+ id + "-numFiles\"></td>";
@@ -114,63 +110,65 @@
     html += "<td> <span id=\""+ id + "-numMissingFiles\"></span></td>";
     html += "<td id=\""+ id + "-nextCheck\"></td></tr>";
     return html;
-  }
+}
 
-  function updateCollectionRow(collection) {
-    var id = collection.collectionID;
+function updateCollectionRow(collection) {
+    let id = collection.collectionID;
     $("#" + id + "-name").html(`<a href='integrity-service.html?collectionID=${collection.collectionID}'>${collection.collectionName}</a>`);
     $("#" + id + "-numFiles").html(numeral(collection.numFiles).format('0,0'));
     $("#" + id + "-latestIngest").html(collection.latestIngest);
     $("#" + id + "-collectionSize").html(collection.collectionSize);
     $("#" + id + "-pillars").html(collection.pillars);
     $("#" + id + "-latestCheck").html(collection.lastCheck);
-    $("#" + id + "-numChecksumErrors").html(numeral(collection.numChecksumErrors).format('0,0'));
-    if(collection.numChecksumErrors > 0) {
-      $("#" + id + "-numChecksumErrors").addClass("badge badge-important pull-right");
+    let checksumErrorsNumberElement = $("#" + id + "-numChecksumErrors");
+    checksumErrorsNumberElement.html(numeral(collection.numChecksumErrors).format('0,0'));
+    if (collection.numChecksumErrors > 0) {
+        checksumErrorsNumberElement.addClass("badge badge-important pull-right");
     } else {
-      $("#" + id + "-numChecksumErrors").removeClass("badge badge-important pull-right");
+        checksumErrorsNumberElement.removeClass("badge badge-important pull-right");
     }
-    $("#" + id + "-numMissingFiles").html(numeral(collection.numMissingFiles).format('0,0'));
-    if(collection.numMissingFiles > 0) {
-      $("#" + id + "-numMissingFiles").addClass("badge badge-important pull-right");
+    let missingFilesNumberElement = $("#" + id + "-numMissingFiles");
+    missingFilesNumberElement.html(numeral(collection.numMissingFiles).format('0,0'));
+    if (collection.numMissingFiles > 0) {
+        missingFilesNumberElement.addClass("badge badge-important pull-right");
     } else {
-      $("#" + id + "-numMissingFiles").removeClass("badge badge-important pull-right");
+        missingFilesNumberElement.removeClass("badge badge-important pull-right");
     }
     $("#" + id + "-nextCheck").html(collection.nextCheck);
-  }
+}
 
-  function refreshContent() {
-      updateWorkflowStatuses();
-      updateInfo();
-      updateStatistics();
-  }
+function refreshContent() {
+    updateWorkflowStatuses();
+    updateInfo();
+    updateStatistics();
+}
 
-  function scheduleUpdate(collection, type) {
-    if(updateLock[collection][type]) {
-      updateLock[collection][type] = false;
-      return true;
+function scheduleUpdate(collection, type) {
+    if (updateLock[collection][type]) {
+        updateLock[collection][type] = false;
+        return true;
     } else {
-      return false;
+        return false;
     }
-  }
+}
 
-  function finishUpdate(collection, type) {
+function finishUpdate(collection, type) {
     return updateLock[collection][type] = true;
-  }
+}
 
-  function initStatusUpdate(updateInterval) {
+function initStatusUpdate(updateInterval) {
     // Init update locks
-    updateLock = new Array();
-    for(c in collections) {
-      var row = new Array();
-      row["status"] = true;
-      row["info"] = true;
-      row["stats"] = true;
-      updateLock[c] = row;
+    updateLock = [];
+    for (let c in collections) {
+        let row = [];
+        row["status"] = true;
+        row["info"] = true;
+        row["stats"] = true;
+        updateLock[c] = row;
     }
     refreshContent();
-    update_page = setInterval(function() { refreshContent(); }, updateInterval);
-  }
+    updatePage = setInterval(function() { refreshContent(); }, updateInterval);
+}
 
 
 
