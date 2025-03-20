b02d2a9cb0a39e7a0b14e66e67d20d33d018984e
RBKR <rbkr@kb.dk>
2025-03-20 16:43:04 +0100

    Add pillar details dto and a small serialization test

---
+++ bitrepository-integrity-service/src/main/java/org/bitrepository/integrityservice/web/PillarDetailsDTO.java
@@ -0,0 +1,67 @@
+package org.bitrepository.integrityservice.web;
+
+import java.io.Serializable;
+
+public class PillarDetailsDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String pillarID;
+    private String pillarName;
+    private String pillarType;
+    private String pillarDeleteFileApprover;
+
+    // Constructors
+    public PillarDetailsDTO() {}
+
+    public PillarDetailsDTO(String pillarID, String pillarName, String pillarType, String pillarDeleteFileApprover) {
+        this.pillarID = pillarID;
+        this.pillarName = pillarName;
+        this.pillarType = pillarType;
+        this.pillarDeleteFileApprover = pillarDeleteFileApprover;
+    }
+
+    // Getters and Setters
+    public String getPillarID() {
+        return pillarID;
+    }
+
+    public void setPillarID(String pillarID) {
+        this.pillarID = pillarID;
+    }
+
+    public String getPillarName() {
+        return pillarName;
+    }
+
+    public void setPillarName(String pillarName) {
+        this.pillarName = pillarName;
+    }
+
+    public String getPillarType() {
+        return pillarType;
+    }
+
+    public void setPillarType(String pillarType) {
+        this.pillarType = pillarType;
+    }
+
+    public String getPillarDeleteFileApprover() {
+        return pillarDeleteFileApprover;
+    }
+
+    public void setPillarDeleteFileApprover(String pillarDeleteFileApprover) {
+        this.pillarDeleteFileApprover = pillarDeleteFileApprover;
+    }
+
+    // toString() Method
+    @Override
+    public String toString() {
+        return "PillarDetailsDTO{" +
+                "pillarID='" + pillarID + '\'' +
+                ", pillarName='" + pillarName + '\'' +
+                ", pillarType=" + pillarType +
+                ", pillarDeleteFileApprover='" + pillarDeleteFileApprover + '\'' +
+                '}';
+    }
+}
