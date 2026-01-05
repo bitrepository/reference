a173f23502164c1ea58ef2744f30f3b99e47ce7d
kaah <kaah@kb.dk>
2026-01-05 17:09:30 +0100

    The suite classes looks for tags instead of classes

--- bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/BitrepositoryChecksumPillarTestSuite.java
+++ bitrepository-reference-pillar/src/test/java/org/bitrepository/pillar/BitrepositoryChecksumPillarTestSuite.java
@@ -1,16 +1,64 @@
 package org.bitrepository.pillar;
 
-import org.junit.platform.suite.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 /**
  * BitrepositoryPillarTestSuite is a JUnit 5 suite class that groups and configures multiple test classes
- * for the BitRepositoryPillar project.
+ * for the BitRepositoryPillar project. This suite uses JUnit 5 annotations to select test classes, packages,
+ * and tags, and extend the suite with custom extensions.
+ *
+ * <p>JUnit 5 Annotations Used:</p>
+ * <ul>
+ *     <li>{@link Suite}: Indicates that this class is a JUnit 5 suite. It groups multiple test classes
+ *     into a single test suite.</li>
+ *     <li>{@link SelectClasses}: Specifies the test classes to be included in the suite. The value is an array
+ *     of class references to the test classes.</li>
+ *     <li>{@link SelectPackages}: Specifies the test packages to be included in the suite. The value is an array
+ *     of package names.</li>
+ *     <li>{@link IncludeTags}: Specifies the tags to include in the suite. The value is an array of tag names.</li>
+ *     <li>{@link ExcludeTags}: Specifies the tags to exclude from the suite. The value is an array of tag names.</li>
+ *     <li>{@link ExtendWith}: Specifies the extensions to be applied to the suite. The value is an array of
+ *     extension classes.</li>
+ * </ul>
+ *
+ * <p>Options in a JUnit 5 Suite:</p>
+ * <ul>
+ *     <li><strong>Selecting Test Classes:</strong> Use the {@link SelectClasses} annotation to specify the test
+ *     classes to be included in the suite. The value is an array of class references to the test classes.</li>
+ *     <li><strong>Selecting Test Packages:</strong> Use the {@link SelectPackages} annotation to specify the test
+ *     packages to be included in the suite. The value is an array of package names.</li>
+ *     <li><strong>Selecting Tests by Tag:</strong> Use the {@link IncludeTags} and {@link ExcludeTags} annotations
+ *     to specify the tags to include or exclude in the suite. The value is an array of tag names.</li>
+ *     <li><strong>Extending the Suite:</strong> Use the {@link ExtendWith} annotation to specify custom extensions
+ *     to be applied to the suite. The value is an array of extension classes.</li>
+ * </ul>
+ *
+ * <p>Example Usage:</p>
+ * <pre>
+ * {@code
+ * @Suite
+ * @SelectClasses({BitrepositoryPillarTest.class})  // List your test classes here
+ * @SelectPackages("org.bitrepository.pillar")  // List your test packages here
+ * @IncludeTags("integration")  // List your include tags here
+ * @ExcludeTags("slow")  // List your exclude tags here
+ * @ExtendWith(GlobalSuiteExtension.class)
+ * public class BitrepositoryTestSuite {
+ *     // No need for methods here; this just groups and extends
+ * }
+ * }
+ * </pre>
  */
-@Suite(failIfNoTests = true)
+@Suite
 @SuiteDisplayName("Checksum Pillar Acceptance Test")
-@SelectPackages({"org.bitrepository.pillar.integration.func"})
-@IncludeTags({PillarTestGroups.CHECKSUM_PILLAR_TEST})
-@IncludeClassNamePatterns(value = "^(Test.*|.+[.$]Test.*|.*Tests?|.*IT)$")
-@ConfigurationParameter(key = "pillarType", value = "Checksum")
+// Select the package(s) where the tests are located
+@SelectPackages("org.bitrepository.pillar.integration.func")
+// Filter to only run methods that have the specific @Tag
+@IncludeTags(PillarTestGroups.CHECKSUM_PILLAR_TEST)
 public class BitrepositoryChecksumPillarTestSuite {
 }
